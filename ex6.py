a741c457ec7f03b61b86eb4e592d1e2856882098
greg mathias <mathiasgreg@gmail.com>
2020-08-21 18:38:06 -0400

    add 9

--- ex6.py
+++ ex6.py
@@ -1,4 +1,6 @@
+# let variable x = the string, with argument specifier %d for number
 x = "There are %d types of people." % 10
+# declare variables to inject into string with argument speifiers
 binary = "binary"
 do_not = "don't"
 y = "Those who know %s and those who %s." % (binary, do_not)
@@ -6,7 +8,7 @@ y = "Those who know %s and those who %s." % (binary, do_not)
 print x
 print y
 
-print "I said: %r." % x 
+print "I said: %r." % x
 print "I also said: '%s'." % y
 
 hilarious = False
