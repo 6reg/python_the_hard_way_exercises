22641b06179e479556535190df95a2f58063618f
greg mathias <mathiasgreg@gmail.com>
2020-09-05 19:02:09 -0400

    ex7 again

--- ex7.py
+++ ex7.py
@@ -1,9 +1,8 @@
-print "Mary had a little lamb."
-print "Its fleece was white as %s." % 'snow'
+print "mary had a little lamb."
+print "its fleece was white as %s." % 'snow'
 print "And everywhere that Mary went."
-print "." * 10 #what'd that do?
+print "." * 10 # what'd that do?
 
-# assigning variables to one letter strings
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -17,7 +16,6 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-# watch that comma at the end. try removing it to see what happens
-# comma keeps next line on same line
+# watch that comma at the end.  try removing it to see what happens
 print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
