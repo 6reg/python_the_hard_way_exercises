8931f4b050d67ad7191ffd12b4c9937d0b29472d
greg mathias <mathiasgreg@gmail.com>
2020-09-13 20:52:46 -0400

    add ex15 again

--- ex15.py
+++ ex15.py
@@ -1,21 +1,15 @@
-# from module "sys", import func or parameter "argv" argv= script name then parameters typed into cmd
 from sys import argv
 
-# accepts two arguments = script and filename
 script, filename = argv
 
-# opens file
 txt = open(filename)
 
 print "Here's your file %r:" % filename
-
-# txt gets read command with no parameters, which prints out contents of txt
 print txt.read()
 
-# take user input to define file_again var then command open with paramter file_a
-#print "Type the filename again:"
-#file_again = raw_input("> ")
+print "Type the filename again:"
+file_again = raw_input("> ")
 
-#txt_again = open(file_again)
+txt_again = open(file_again)
 
-#print txt_again.read()
+print txt_again.read()
