bad774831db1db5cc8746d8b2857c51dc4df9282
greg mathias <mathiasgreg@gmail.com>
2020-09-27 20:12:32 -0400

    ex19

--- ex19.py
+++ ex19.py
@@ -1,32 +1,20 @@
-# function block starts with keyword def followed by name of
-#function and parenthesis containing to parameters / aragumnents
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
-    # function body  is
-    # statements defining the function that include strings with the args
-    # injected with placeholders
     print "You have %d cheeses!" % cheese_count
-    print "YOU have %d boxes of crackers!" % boxes_of_crackers
+    print "You have %d boxes of crackers!" % boxes_of_crackers
     print "Man that's enough for a party!"
     print "Get a blanket.\n"
 
-
 print "We can just give the function numbers directly:"
-# function call with two ints as args
 cheese_and_crackers(20, 30)
 
-
 print "OR, we can use variables from our script:"
-# variables assigned int values
 amount_of_cheese = 10
 amount_of_crackers = 50
-# function call with vars
+
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
-
 print "We can even do math inside too:"
-# func called with math passed as args
 cheese_and_crackers(10 + 20, 5 + 6)
-
 
 print "And we can combine the two, variables and math:"
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
--- ex20.py
+++ ex20.py
@@ -1,20 +1,31 @@
+# import argv from sys
 from sys import argv
 
+# get arguments
 script, input_file = argv
 
+# define print_all function, which takes one arg (f)
 def print_all(f):
+    # print the contents of f
     print f.read()
 
+# define rewind function, which takes one arg (f)
 def rewind(f):
+    # move the file pointer back to the beginning of the file f
     f.seek(0)
 
+# define the print_a_line function, which takes 2 args (line_count, f)
 def print_a_line(line_count, f):
+    # print line_count, then a line from file f
     print line_count, f.readline()
 
+# set current_file variable to a file pointer to input_file
 current_file = open(input_file)
 
+# print string
 print "First let's print the whole file:\n"
 
+# call print_all function with current_file as an arg
 print_all(current_file)
 
 print "Now let's rewind, kind of like a tape."
