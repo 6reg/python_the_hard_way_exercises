ca8b6eee0ef74703ad56d3a958e2e185ad59c8a6
greg mathias <mathiasgreg@gmail.com>
2020-09-29 22:21:03 -0400

    add 17 aggaiinn

--- ex17.py
+++ ex17.py
@@ -3,15 +3,15 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-# print "Copying from %s to %s" % (from_file, to_file)
+print "Copying from %s to %s" % (from_file, to_file)
 
-# we could do these two on one line too, how?
+# we could d these two on one line too, how?
 in_file = open(from_file)
 indata = in_file.read()
 
 print "The input file is %d bytes long" % len(indata)
 
-print "Does the output file exist? %r" % exists(to_file)
+print "Does the ouput file exist? %r" % exists(to_file)
 print "Ready, hit RETURN to continue, CTRL-C to abort."
 raw_input()
 
