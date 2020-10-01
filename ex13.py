9d47533f41d0f0aa24542879a35dd7c7178974f8
greg mathias <mathiasgreg@gmail.com>
2020-09-30 22:04:37 -0400

    ex13 aaaggggaaaiiinnn

--- ex13.py
+++ ex13.py
@@ -3,6 +3,9 @@ from sys import argv
 script, first, second, third = argv
 
 print "The script is called:", script
+print "What is the script called?"
+y = raw_input("Script name ")
+print "Ok, you think the script is called %r." % y
 print "Your first variable is:", first
-print "Your second variable is:", second
-print "Your third variable is:", 3rd
+print "Your sceond variable is:", second
+print "Your third variable is:", third
