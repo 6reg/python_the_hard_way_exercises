eb1fea591d8d01ac023c215da07b88413c3c14e9
greg mathias <mathiasgreg@gmail.com>
2020-10-07 22:27:18 -0400

    add 21 again

--- ex21.py
+++ ex21.py
@@ -24,7 +24,7 @@ iq = divide(100, 2)
 print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
 
 # A puzzle for the extra credit, type it in anyway.
-print "Here is a puzzle."
+print "here is a puzzle."
 
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
