6d0df337fd51807b964665cb4ecbbd6f03d8f7bc
greg mathias <mathiasgreg@gmail.com>
2020-11-20 22:00:34 -0500

    Ex20 vimmed tf out

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,1 +1,12 @@
+// prompt user for a number then store it in variable 'answer', wrap it in parseInt function to ensure it's a number
+
 let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to."));
+
+for (let i = 1; i <= answer; i++) {
+	if (i % 3 === 0) && (i % 5 === 0) {
+		console.log("FizzBuzz");
+	}else if (i % 3 === 0) {
+		console.log("Fizz");
+	} else {
+		console.log(i);
+	};
