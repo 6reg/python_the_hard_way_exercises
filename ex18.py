b21e0e13016a34caec2ec01e8931371f8c1ed719
greg mathias <mathiasgreg@gmail.com>
2020-12-02 20:38:52 -0500

    Ex18.py

--- ex18.py
+++ ex18.py
@@ -3,7 +3,7 @@ def print_two(*args):
     arg1, arg2 = args
     print "arg1: %r, arg2: %r" % (arg1, arg2)
 
-# ok, that *args is actually pointless, we can just do this
+#ok, that *args is actually pointless, we can just do this
 def print_two_again(arg1, arg2):
     print "arg1: %r, arg2: %r" % (arg1, arg2)
 
@@ -15,8 +15,7 @@ def print_one(arg1):
 def print_none():
     print "I got nothin'."
 
-
-print_two("6reg", "Mathias")
-print_two_again("6reg", "Mathias")
-print_one("First!")
+print_two(2, 5)
+print_two_again(5, 2)
+print_one("dill")
 print_none()
