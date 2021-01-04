a946c81fce79cc80c5a53de5338deab4d3ff357b
greg mathias <mathiasgreg@gmail.com>
2021-01-03 20:27:37 -0500

    Ex40.py

--- ex40.py
+++ ex40.py
@@ -1,19 +1,14 @@
 class Song(object):
+
     def __init__(self, lyrics):
         self.lyrics = lyrics
 
-    def print_lyrics(self):
-        for i in self.lyrics:
-            print(i)
-amelia_lyrics = ["I was driving across",
-                "A burning desert",
-                "When I spotted six"]
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
 
-amelia = Song(amelia_lyrics)
+happy_bday = Song(["happy bday to you",
+                    "lalala ",
+                    "llaallaa"])
 
-in_your_eyes = Song(["love",
-                    "I get so lost",
-                    "Sometimes"])
-
-amelia.print_lyrics()
-in_your_eyes.print_lyrics()
+happy_bday.sing_me_a_song()
