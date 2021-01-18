4615b3a78ad799811003aafe2f93714e8ae807b8
greg mathias <mathiasgreg@gmail.com>
2021-01-18 18:09:28 -0500

    Game

--- game.py
+++ game.py
@@ -1,15 +1,28 @@
+from sys import exit
+from random import randint
+from textwrap import dedent
+
 class Scene(object):
 
     def enter(self):
-        pass
+        print("This scene is not yet configured.")
+        print("Subclass it and implement enter().")
 
 class Engine(object):
 
     def __init__(self, scene_map):
-        pass
+        self.scene_map = scene-map 
 
     def play(self):
-        pass
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
+
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+
+            # be sure to print out the last scene
+            current_scene.enter
 
 class Home(Scene):
 
