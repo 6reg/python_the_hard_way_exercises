acb59c565fb29bc6e691f5eec3c2cb748fb91cf0
greg mathias <mathiasgreg@gmail.com>
2021-02-16 23:19:43 -0500

    Nudu

--- ex20.py
+++ ex20.py
@@ -1,33 +1,15 @@
-from sys import argv
+def add (a, b):
+    print(f"adding {a} + {b}")
+    return a + b
 
-script, input_file = argv
+def subtract(a, b):
+    print(f"subtracting {a] - {b}")
+    return a - b
 
-def print_all(f):
-    print(f.read())
+def multipy(a, b):
+    print(f"multiply {a} & * {b}")
+    return a * b
 
-def rewind(f):
-    f.seek(0)
+age = add(30, 5)
 
-def print_a_line(line_count, f):
-    print(line_count, f.readline())
-
-current_file = open(input_file)
-
-print("First let's print the whole file:\n")
-
-print_all(current_file)
-
-print("Now let's rewind, kind of like a tape.")
-
-rewind(current_file)
-
-print("Let's print three lines:")
-
-current_line = 1
-print_a_line(current_line, current_file)
-
-current_line = current_line + 1
-print_a_line(current_line, current_file)
-
-current_line = current_line + 1
-print_a_line
+what = add(5, 5(5, 5(5, 5(5, 5))))
