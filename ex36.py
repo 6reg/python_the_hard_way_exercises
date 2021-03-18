ffea18eee7d97722fac76ba647e29e2240d87e7b
greg mathias <mathiasgreg@gmail.com>
2021-03-18 17:40:18 -0400

    ex35.py

--- ex36.py
+++ ex36.py
@@ -1,1 +1,13 @@
-import stdin
+
+
+def begin():
+    print("You look out at a snowy peak.")
+    print("You ponder whether to grab your skiis or your board.")
+    print("Which do you grab?")
+
+    grab = input("Skiis or Board?")
+
+    if "Skiis" in grab:
+        skiing()
+    elif "Board" in grab:
+        boarding()
