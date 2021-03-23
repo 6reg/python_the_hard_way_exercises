fcca160594169b180dc7ecedbe8f029576b4ab8a
greg mathias <mathiasgreg@gmail.com>
2021-03-22 21:52:00 -0400

    Words

---
+++ conways1.py
@@ -0,0 +1,14 @@
+# Conway's Game of Life
+import random, time, copy
+WIDTH = 60
+HEIGHT = 20
+
+# Create a list of list for the cells:
+nextCells = []
+for x in range(WIDTH):
+    column = [] # Create a new column.
+    for y in range(HEIGHT):
+        if random.randint(0, 1) == 0:
+            column.append('O') # Add a living cell. 
+        else:
+            column.append('  ') # Add a dead cell.
