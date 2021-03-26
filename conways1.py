581eb06791024fc84cca894a2d8dc723ce9cef5a
greg mathias <mathiasgreg@gmail.com>
2021-03-25 20:33:47 -0400

    Conways

--- conways1.py
+++ conways1.py
@@ -21,4 +21,12 @@ while True: # Main program loop.
     # Print currentCells on the screen:
     for y in range(HEIGHT):
         for x in range(WIDTH):
-            print(currrentCells[x][y], end='') # Print the 'O' or space
+            print(currrentCells[x][y], end='') # Print the 'O' or space.
+        print() # Print a newline at the end of the row.
+
+    # Calculate next step's cells based on current step's cells:
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            # Get neighboring coordinates:
+            # '% WIDTH' ensures leftCoord is always between 0 and WIDTH -1
+            LeftCoord = (x - 1) % WIDTH
