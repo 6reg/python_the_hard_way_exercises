d3cd464e0734fc68cc47fd89ef137d780ff3224a
greg mathias <mathiasgreg@gmail.com>
2021-03-26 19:40:21 -0400

    Conways

--- conways1.py
+++ conways1.py
@@ -29,4 +29,15 @@ while True: # Main program loop.
         for y in range(HEIGHT):
             # Get neighboring coordinates:
             # '% WIDTH' ensures leftCoord is always between 0 and WIDTH -1
-            LeftCoord = (x - 1) % WIDTH
+            leftCoord = (x - 1) % WIDTH
+            rightCoord = (x + 1) % WIDTH
+            aboveCoord = (y - 1) % HEIGHT
+            belowCoord = (y + 1) % HEIGHT
+
+            # Count number of living neighbors: 
+            numNeighbors = 0
+            if currentCells[leftCoord][aboveCoord] == '#':
+                numNeighbors += 1 # Top-left neighbor is alive.
+            if currentCells[x][aboveCoord] == '#':
+                
+
