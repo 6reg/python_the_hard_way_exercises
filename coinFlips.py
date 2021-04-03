13d700aa8e9cefa5a77fef230d8057645b180731
greg mathias <mathiasgreg@gmail.com>
2021-04-02 21:53:08 -0400

    Coin

--- coinFlips.py
+++ coinFlips.py
@@ -4,10 +4,11 @@ for experimentNumber in range(10000):
     # Code that creates a list of 100 'heads' or 'tails' values.
     listOfHeadsOrTails = []
     flip = random.randint(0, 1) 
-    if flip == 0:
-        listOfHeadsOrTails += "H"
-    else:
-        listOfHeadsOrTails += "T"
+    for i in range(100):
+        if flip == 0:
+         listOfHeadsOrTails += "H"
+        else:
+         listOfHeadsOrTails += "T"
 
     if listOfHeadsOrTails[n] == listOfHeadsOrTails[n+1]
     # Code that checks if there is a streak of 6 heads or tails in a row. 
