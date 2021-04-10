3391834fa667532e985761d05fae3896027bbf37
greg mathias <mathiasgreg@gmail.com>
2021-04-09 20:23:11 -0400

    CharGrid

--- charPicGrid.py
+++ charPicGrid.py
@@ -11,4 +11,3 @@ grid = [['.', '.', '.', '.', '.', '.'],
 
 for x in range(6):
     for y in range(9):
-        
--- ex39.py
+++ ex39.py
@@ -30,17 +30,17 @@ print("Florida's abbreviation is: ", states['Florida'])
 
 # do it by using the state then cities dict
 print('-' * 10)
-print("Michigan has: ", cities[states['Mickigan']]
+print("Michigan has: ", cities[states['Michigan']])
 print("Florida has: ", cities[states['Florida']])
 
 # print every state abbreviation
 print('-' * 10)
-for state, abbrev in list(states.items()): 
+for state, abbrev in list(states.items()):
     print(f"{state} is abbreviated {abbrev}")
 
 # print every city in state
 print('-' * 10)
-for abbrev, city in list(cities.items()):
+for abbrev,  city in list(cities.items()):
     print(f"{abbrev} has the city {city}")
 
 # now do both at the same time
@@ -48,3 +48,15 @@ print('-' * 10)
 for state, abbrev in list(states.items()):
     print(f"{state} state is abbreviated {abbrev}")
     print(f"and has city {cities[abbrev]}")
+
+print('-' * 10)
+# safely get an abbreviation by state that might not be there
+state = states.get('Texas')
+
+if not state: 
+    print("Sorry, no Texas.")
+
+# ge a city with a default value
+city = cities.get('TX', 'Does Not Exist')
+print(f"The city for the state 'TX' is: {city}")
+
